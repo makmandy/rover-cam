504ef98ccb5fd90471f220556078e6b1ee90fded
Mandy Mak <mandymak.me@gmail.com>
2019-11-19 12:37:58 -0800

    Pseudocode rough plan of the app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>rovercam</h1>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    date: '',
+    photoList: [],
+  }
+  
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>rovercam</h1>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+// fetch photos of today and render on load
+// when date is changed, use the date to search photos of the selected date
+// update state (photos) and pass to list of photos
+
+// initial state includes:
+  // photoList
+  // date
+
+// components in app
+  // date selector
+  // photogrid
+
+// PhotoGrid
+  // compose of Photo components
+
+// 
