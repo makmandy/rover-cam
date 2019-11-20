ceec88815c308d517f78895a06bce266ee149663
Mandy Mak <mandymak.me@gmail.com>
2019-11-20 13:29:58 -0800

    Refactor Photo to stateless component

--- src/Photo/Photo.js
+++ src/Photo/Photo.js
@@ -1,38 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import './Photo.css';
 
-class Photo extends Component {
-  // add styling 
-  // maybe metadata should be an overlay 
-  render() {
-    // const { photo } = this.props;
-    const {
-      id,
-      cameraName,
-      roverName,
-      imageSrc,
-    } = this.props;
-    return (
-      <div className="photoContainer">
-        <img
-          // alt={`marsRoverPhoto${photo.id}`}
-          alt={`marsRoverPhoto${id}`}
-          src={imageSrc}
-          className="image"
-        />
-        <div className="overlay">
-          <div className="metadata">
-            {cameraName}
-            <br/>
-            ({roverName} Rover)
-          </div>
-        </div>
+const Photo = ({
+  id,
+  cameraName,
+  roverName,
+  imageSrc,
+}) => (
+  <div className="photoContainer">
+    <img
+      alt={`marsRoverPhoto${id}`}
+      src={imageSrc}
+      className="image"
+    />
+    <div className="overlay">
+      <div className="metadata">
+        {cameraName}
+        <br/>
+        ({roverName} Rover)
       </div>
-    );
-  }
-}
+    </div>
+  </div>
+  );
 
 Photo.propTypes = {
   id: PropTypes.string,
