f1252301a2c32bf58d0bbdb9281db16df1d54c40
Mandy Mak <mandymak.me@gmail.com>
2019-11-20 13:31:57 -0800

    Refactor DateSelector to stateless component

--- src/DateSelector/DateSelector.js
+++ src/DateSelector/DateSelector.js
@@ -1,31 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import './DateSelector.css';
 
-class DateSelector extends Component {
-  render() {
-    const {
-      date,
-      fetchPhotosByDate,
-      handleChangeDate,
-    } = this.props;
-
-    return (
-      <div>
-      <input
-        type="date"
-        className="dateSelector"
-        value={date}
-        onChange={handleChangeDate}
-      />
-      <button
-        className="button"
-        onClick={fetchPhotosByDate}>&rarr;</button>
-      </div>
-    )
-  }
-}
+const DateSelector = ({
+  date,
+  fetchPhotosByDate,
+  handleChangeDate,
+}) => (
+  <div>
+    <input
+      type="date"
+      className="dateSelector"
+      value={date}
+      onChange={handleChangeDate}
+    />
+    <button
+      className="button"
+      onClick={fetchPhotosByDate}
+    >
+      &rarr;
+    </button>
+  </div>
+);
 
 DateSelector.propTypes = {
   date: PropTypes.string,
