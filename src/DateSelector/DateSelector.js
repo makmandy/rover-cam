6f44cfcf3944248061208e5a274b9101f0672b96
Mandy Mak <mandymak.me@gmail.com>
2019-11-20 10:52:30 -0800

    Add styling to DateSelector to look like previous date display

--- src/DateSelector/DateSelector.js
+++ src/DateSelector/DateSelector.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment-timezone';
 
+import './DateSelector.css';
+
 class DateSelector extends Component {
   state = {
+    value: this.props.date,
   }
 
-  handleChangeDate = (date) => {
+  handleChangeDate = (e) => {
+    this.setState({
+      value: e.target.value
+    }, () => console.log(this.state.value));
 
   }
 
@@ -14,8 +20,9 @@ class DateSelector extends Component {
     const { date } = this.props;
     const prettyDate = moment(date).format('MMMM DD, YYYY');
     return (
-      <div className="dateSelector">
+      <div>
       <h3>{prettyDate}</h3>
+      <input type="text" placeholder="MM/DD/YYYY" className="dateSelector" value={this.state.value} onChange={this.handleChangeDate} />
       </div>
     )
   }
